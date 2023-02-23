13afdb942714610ca979808761a2954b76039212
abalmasDA <abalmasit@gmail.com>
2023-02-23 19:14:46 +0200

    JavaStart

---
+++ src/ua/com/foxminded/javastart/HelloWorld.java
@@ -0,0 +1,51 @@
+package ua.com.foxminded.javastart;
+
+public class HelloWorld {
+    public static void main(String[] args){
+//        String message = "Hello World!";
+//        String words = "Java forever";
+//        String space = " ";
+//        String number = "-2";
+        int i = 6;
+        int j = 7;
+//        float answer = (float)i/(float)j;
+//        double d = 2.5;
+//
+//        int hugeNumber = 521124244;
+//
+//        boolean isMoreThanOne;
+//
+//        if (answer>1) {
+//            isMoreThanOne = true;
+//        } else {
+//            isMoreThanOne = false;
+//        }
+
+        boolean evenNumber;
+
+        if (i%2 == 0){
+            evenNumber = true;
+        } else {
+            evenNumber = false;
+        }
+        System.out.println(evenNumber);
+
+        if (j%2 == 0){
+            evenNumber = true;
+        } else {
+            evenNumber = false;
+        }
+        System.out.println(evenNumber);
+
+
+
+//        int result = answer+Integer.parseInt(number);
+//        String oneMoreAnswer = message + space + result;
+
+
+//        System.out.println(oneMoreAnswer);
+//        System.out.println(isMoreThanOne);
+
+    }
+
+}
